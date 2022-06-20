bb36a665bfbf5e7d52f3f38310696bc1fd65f583
joshres <97273313+joshres@users.noreply.github.com>
2022-06-20 14:47:20 -0700

    working on search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+function search_bar() {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    let images = document.getElementsByClassName('image');
+
+    for (i = 0; i < images.length; i++) {
+        let image_title = images[i].getAttribute("alt").toLowerCase();
+
+        if (!image_title.includes(input)) {
+            images[i].style.display="none";
+        } else {
+            images[i].style.display="";
+        }
+
+    }
+}
+
+
+
+
